cd52814210661c17b3bd57c0d839435cc623acbc
Connor Carpenter <connorcarpenter@gmail.com>
2023-04-03 15:32:42 -0500

    cleanup

---
+++ crates/render_glow/src/window/winit_window/error.rs
@@ -0,0 +1,47 @@
+use thiserror::Error;
+
+use crate::core::CoreError;
+
+///
+/// Error associated with a window.
+///
+#[cfg(not(target_arch = "wasm32"))]
+#[derive(Error, Debug)]
+#[allow(missing_docs)]
+pub enum WindowError {
+    #[error("glutin error")]
+    GlutinError(#[from] glutin::error::Error),
+    #[error("winit error")]
+    WinitError(#[from] winit::error::OsError),
+    #[error("error in three-d")]
+    ThreeDError(#[from] CoreError),
+    #[error("the number of MSAA samples must be a power of two")]
+    InvalidNumberOfMSAASamples,
+    #[error("it's not possible to create a graphics context/surface with the given settings")]
+    SurfaceCreationError,
+}
+
+///
+/// Error associated with a window.
+///
+#[cfg(target_arch = "wasm32")]
+#[derive(Error, Debug)]
+#[allow(missing_docs)]
+pub enum WindowError {
+    #[error("failed to create a new winit window")]
+    WinitError(#[from] winit::error::OsError),
+    #[error("failed creating a new window")]
+    WindowCreation,
+    #[error("unable to get document from canvas")]
+    DocumentMissing,
+    #[error("unable to convert canvas to html canvas: {0}")]
+    CanvasConvertFailed(String),
+    #[error("unable to get webgl2 context for the given canvas, maybe the browser doesn't support WebGL2{0}")]
+    WebGL2NotSupported(String),
+    #[error("unable to get EXT_color_buffer_float extension for the given canvas, maybe the browser doesn't support EXT_color_buffer_float: {0}")]
+    ColorBufferFloatNotSupported(String),
+    #[error("unable to get OES_texture_float extension for the given canvas, maybe the browser doesn't support OES_texture_float: {0}")]
+    OESTextureFloatNotSupported(String),
+    #[error("error in three-d")]
+    ThreeDError(#[from] CoreError),
+}
