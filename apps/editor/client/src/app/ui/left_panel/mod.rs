80b9bb8954f89be94b873f47ba61ac1bca1141c6
Connor Carpenter <connorcarpenter@gmail.com>
2023-04-09 16:18:34 -0500

    left panel working right

--- apps/editor/client/src/app/ui/left_panel/mod.rs
+++ apps/editor/client/src/app/ui/left_panel/mod.rs
@@ -13,34 +13,32 @@ pub fn left_panel(
         .resizable(true)
         .default_width(150.0)
         .show(context, |ui| {
-            // Left Bottom Panel
-            egui::TopBottomPanel::bottom("left_bottom_panel")
+
+            // Left Top Panel
+            egui::TopBottomPanel::top("left_top_panel")
                 .resizable(true)
+                .show_inside(ui, |ui| {
+                    ui.heading("Project");
+                    ui.separator();
+                    egui::ScrollArea::vertical()
+                        .auto_shrink([false, false])
+                        .show(ui, |ui| {
+                            let mut tree = world.get_resource_mut::<ProjectTree>().unwrap();
+                            tree.0.ui(ui);
+                        });
+                });
+            // Left Bottom Panel
+            egui::CentralPanel::default()
                 .show_inside(ui, |ui| {
                     ui.heading("Git Changes");
                     ui.separator();
                     egui::ScrollArea::vertical()
                         .auto_shrink([false, false])
-                        .always_show_scroll(true)
                         .show(ui, |ui| {
                             let mut tree = world.get_resource_mut::<ProjectTree>().unwrap();
                             tree.0.ui(ui);
                             ui.allocate_space(ui.available_size());
                         });
                 });
-            // Left Top Panel
-            egui::CentralPanel::default()
-                .show_inside(ui, |ui| {
-                    ui.heading("Project");
-                    ui.separator();
-                    egui::ScrollArea::vertical()
-                        .auto_shrink([true, true])
-                        .always_show_scroll(true)
-                        .show(ui, |ui| {
-                            let mut tree = world.get_resource_mut::<ProjectTree>().unwrap();
-                            tree.0.ui(ui);
-                        });
-                });
-
         });
 }
