f44ca167b706fb7e1f9fc3853a8be913e79c7afe
Connor Carpenter <connorcarpenter@gmail.com>
2023-04-11 14:47:29 -0500

    login to server through modal works

--- apps/vortex/client/src/app/systems/network/connect.rs
+++ apps/vortex/client/src/app/systems/network/connect.rs
@@ -1,11 +1,14 @@
-use bevy_ecs::event::EventReader;
+use bevy_ecs::{event::EventReader, system::ResMut};
 use bevy_log::info;
 
 use naia_bevy_client::{events::ConnectEvent, Client};
 
-pub fn connect_events(client: Client, mut event_reader: EventReader<ConnectEvent>) {
+use crate::app::ui::UiState;
+
+pub fn connect_events(client: Client, mut event_reader: EventReader<ConnectEvent>, mut state: ResMut<UiState>) {
     for _ in event_reader.iter() {
         let server_address = client.server_address().unwrap();
         info!("Client connected to: {}", server_address);
+        state.logged_in = true;
     }
 }
--- apps/vortex/client/src/app/systems/network/disconnect.rs
+++ apps/vortex/client/src/app/systems/network/disconnect.rs
@@ -1,10 +1,13 @@
-use bevy_ecs::event::EventReader;
+use bevy_ecs::{event::EventReader, system::ResMut};
 use bevy_log::info;
 
 use naia_bevy_client::events::DisconnectEvent;
 
-pub fn disconnect_events(mut event_reader: EventReader<DisconnectEvent>) {
+use crate::app::ui::UiState;
+
+pub fn disconnect_events(mut event_reader: EventReader<DisconnectEvent>, mut state: ResMut<UiState>) {
     for _ in event_reader.iter() {
         info!("Client disconnected from Server");
+        state.logged_in = false;
     }
 }
