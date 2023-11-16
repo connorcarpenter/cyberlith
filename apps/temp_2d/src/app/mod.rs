f421af8f6b58c5d3d929dd3d840dc3c521450410
Connor Carpenter <connorcarpenter@gmail.com>
2023-11-15 21:36:14 -0600

    get rid of temp

--- apps/temp_2d/src/app/mod.rs
+++
@@ -1,9 +0,0 @@
-pub use build::build;
-pub use plugins::*;
-pub use systems::*;
-
-mod build;
-mod components;
-mod plugins;
-mod resources;
-mod systems;
--- apps/temp_2d/src/app/plugins/mod.rs
+++
@@ -1,3 +0,0 @@
-pub use game::GamePlugin;
-
-mod game;
