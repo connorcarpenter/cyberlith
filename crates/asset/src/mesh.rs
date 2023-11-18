9393289f9e221e030434eed43c5dcb12732e8577
Connor Carpenter <connorcarpenter@gmail.com>
2023-11-17 21:05:55 -0600

    flat shading on mesh file

--- crates/asset/src/mesh.rs
+++ crates/asset/src/mesh.rs
@@ -32,7 +32,7 @@ impl From<MeshFile> for CpuMesh {
         let mesh_actions = filetypes::MeshAction::read(&mut bit_reader).expect("unable to read mesh file");
 
         let mut vertices = Vec::new();
-        let mut indices = Vec::new();
+        let mut positions = Vec::new();
         for action in mesh_actions {
             match action {
                 filetypes::MeshAction::Vertex(x, y, z) => {
@@ -42,17 +42,20 @@ impl From<MeshFile> for CpuMesh {
                 }
                 filetypes::MeshAction::Face(face_id, vertex_a_id, vertex_b_id, vertex_c_id, _, _, _) => {
                     println!("Face: {}, {}, {}, {}", face_id, vertex_a_id, vertex_b_id, vertex_c_id);
-                    indices.push(vertex_a_id);
-                    indices.push(vertex_b_id);
-                    indices.push(vertex_c_id);
+                    let vertex_a = vertices[vertex_a_id as usize];
+                    let vertex_b = vertices[vertex_b_id as usize];
+                    let vertex_c = vertices[vertex_c_id as usize];
+
+                    positions.push(vertex_a);
+                    positions.push(vertex_b);
+                    positions.push(vertex_c);
                 }
                 _ => {}
             }
         }
 
         let mut mesh = CpuMesh {
-            indices: Indices(Some(indices)),
-            positions: Positions(vertices),
+            positions: Positions(positions),
             ..Default::default()
         };
 
