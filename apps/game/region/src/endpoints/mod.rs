dbdd5160382328e594afddb0aedb488c0f1cb29d
Connor Carpenter <connorcarpenter@gmail.com>
2024-01-26 16:05:30 -0600

    reorg

--- apps/game/region/src/endpoints/mod.rs
+++ apps/game/region/src/endpoints/mod.rs
@@ -1,11 +1,6 @@
-mod session_register_instance;
-pub use session_register_instance::*;
 
-mod world_register_instance;
-pub use world_register_instance::*;
+mod session;
+mod world;
 
-mod world_user_login;
-pub use world_user_login::*;
-
-mod session_user_login;
-pub use session_user_login::*;
+pub use session::*;
+pub use world::*;
---
+++ apps/game/region/src/endpoints/world/mod.rs
@@ -0,0 +1,5 @@
+mod register_instance;
+mod user_login;
+
+pub use register_instance::*;
+pub use user_login::*;
