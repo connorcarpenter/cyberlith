43560ed097cd13b8ee583ca58594d711649e7a96
Connor Carpenter <connorcarpenter@gmail.com>
2024-02-08 17:14:40 -0600

    reading file contents

--- apps/automation/lib/src/process_assets/json.rs
+++ apps/automation/lib/src/process_assets/json.rs
@@ -1,4 +1,6 @@
 use std::{fs, path::Path};
+use std::fs::File;
+use std::io::Read;
 
 use git2::{Cred, Repository, Tree};
 use log::info;
@@ -9,7 +11,9 @@ pub(crate) fn process_assets() -> Result<(), CliError> {
     info!("Processing assets: 'json'");
 
     // pull all assets into memory, from "main" branch
-    create_project();
+    let root = "target/repo";
+    let repo = repo_init(root);
+    let files = load_all_files(root, &repo);
 
     // switch to "json" branch
 
@@ -22,9 +26,8 @@ pub(crate) fn process_assets() -> Result<(), CliError> {
     Ok(())
 }
 
-fn create_project() {
+fn repo_init(root_dir: &str) -> Repository {
     // Create Working directory if it doesn't already exist
-    let root_dir = "target/repo";
     let path = Path::new(&root_dir);
     let repo_url = include_str!("../../../../../.secrets/assets_repo_url");
     let access_token = include_str!("../../../../../.secrets/github_token");
@@ -72,19 +75,34 @@ fn create_project() {
                 Some(&mut checkout_builder),
             )
                 .unwrap();
-
-            info!("pulled repo with new changes");
         }
+
+        info!("pulled repo with new changes");
 
         repo
     };
 
-    {
-        let head = repo.head().unwrap();
-        let tree = head.peel_to_tree().unwrap();
+    repo
+}
 
-        fill_file_entries_from_git(&repo, &tree, "");
-    }
+#[derive(Debug)]
+struct FileEntry {
+    path: String,
+    name: String,
+    file_ext: String,
+    bytes: Vec<u8>,
+}
+
+fn load_all_files(root: &str, repo: &Repository) -> Vec<FileEntry> {
+
+    let mut output = Vec::new();
+
+    let head = repo.head().unwrap();
+    let tree = head.peel_to_tree().unwrap();
+
+    fill_file_entries_from_git(&mut output, root, &repo, &tree, "");
+
+    output
 }
 
 fn get_remote_callbacks(access_token: &str) -> git2::RemoteCallbacks {
@@ -97,14 +115,14 @@ fn get_remote_callbacks(access_token: &str) -> git2::RemoteCallbacks {
 }
 
 fn fill_file_entries_from_git(
+    output: &mut Vec<FileEntry>,
+    root: &str,
     repo: &Repository,
     git_tree: &Tree,
     path: &str,
 ) {
     for git_entry in git_tree.iter() {
         let name = git_entry.name().unwrap().to_string();
-
-        info!("Git -> Tree: processing Entry `{:?}`", name);
 
         match git_entry.kind() {
             Some(git2::ObjectType::Tree) => {
@@ -114,6 +132,8 @@ fn fill_file_entries_from_git(
                 let git_children = git_entry.to_object(repo).unwrap().peel_to_tree().unwrap();
 
                 fill_file_entries_from_git(
+                    output,
+                    root,
                     repo,
                     &git_children,
                     &new_path,
@@ -121,10 +141,42 @@ fn fill_file_entries_from_git(
             }
             Some(git2::ObjectType::Blob) => {
 
+                let file_ext = name.split(".").last().unwrap();
+                let bytes = get_file_contents(root, path, &name);
+
+                let file_entry = FileEntry {
+                    path: path.to_string(),
+                    name: name.to_string(),
+                    file_ext: file_ext.to_string(),
+                    bytes,
+                };
+
+                info!("read file: {}, byte_len: {}", file_entry.name, file_entry.bytes.len());
+
+                output.push(file_entry);
             }
             _ => {
                 info!("Unknown file type: {:?}", git_entry.kind());
             }
         }
+    }
+}
+
+fn get_file_contents(root: &str, path: &str, file: &str) -> Vec<u8> {
+    let file_path = format!("{}{}", path, file);
+    let full_path = format!("{}/{}", root, file_path);
+
+    // info!("Getting blob for file: {}", full_path);
+
+    let path = Path::new(full_path.as_str());
+    let mut file = match File::open(path) {
+        Ok(file) => file,
+        Err(err) => panic!("Failed to open file: {}", err),
+    };
+
+    let mut contents = Vec::new();
+    match file.read_to_end(&mut contents) {
+        Ok(_) => contents,
+        Err(err) => panic!("Failed to read file: {}", err),
     }
 }
