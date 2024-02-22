c41ffb582889979ed762645bc5db044ebb8e9b1e
Connor Carpenter <connorcarpenter@gmail.com>
2024-02-22 13:46:59 -0600

    update/stubout wasm filesystem api

---
+++ crates/filesystem/src/wasm/functions/create_dir.rs
@@ -0,0 +1,5 @@
+use std::{path::Path, io};
+
+pub fn create_dir<P: AsRef<Path>>(path: P) -> io::Result<()> {
+    todo!()
+}
--- crates/filesystem/src/wasm/types/read_dir.rs
+++ crates/filesystem/src/wasm/types/read_dir.rs
@@ -1,3 +1,26 @@
-pub struct ReadDir {}
+
+use std::io;
+
+pub struct ReadDir {
+
+}
 
 impl ReadDir {}
+
+impl Iterator for ReadDir {
+    type Item = io::Result<DirEntry>;
+
+    fn next(&mut self) -> Option<io::Result<DirEntry>> {
+        todo!()
+    }
+}
+
+pub struct DirEntry {
+
+}
+
+impl DirEntry {
+    pub fn path(&self) -> std::path::PathBuf {
+        todo!()
+    }
+}
