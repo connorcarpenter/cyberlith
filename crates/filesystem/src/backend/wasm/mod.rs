92e348cf35a207ee5d2c19dbf6a76be7f22bbb66
Connor Carpenter <connorcarpenter@gmail.com>
2024-02-22 20:05:26 -0600

    stubbing out writes

--- crates/filesystem/src/backend/wasm/mod.rs
+++ crates/filesystem/src/backend/wasm/mod.rs
@@ -5,9 +5,9 @@ use js_sys::{Array, AsyncIterator, Function, Promise};
 use log::info;
 use wasm_bindgen::JsValue;
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{FileSystemDirectoryHandle, FileSystemGetDirectoryOptions};
+use web_sys::{FileSystemDirectoryHandle, FileSystemGetDirectoryOptions, FileSystemGetFileOptions, FileSystemFileHandle, FileSystemWritableFileStream};
 
-use crate::{tasks::{write::WriteTask, read_dir::{ReadDirTask, ReadDirEntry}, read::ReadTask, create_dir::CreateDirTask, task_enum::{FsTaskEnum, FsTaskResultEnum}}, error::TaskError, ReadDirResult, CreateDirResult};
+use crate::{tasks::{write::WriteTask, read_dir::{ReadDirTask, ReadDirEntry}, read::ReadTask, create_dir::CreateDirTask, task_enum::{FsTaskEnum, FsTaskResultEnum}}, error::TaskError, ReadDirResult, CreateDirResult, WriteResult};
 
 pub(crate) struct FsTaskJob(pub Receiver<Result<FsTaskResultEnum, TaskError>>);
 
@@ -51,7 +51,21 @@ async fn handle_read(task: &ReadTask) -> Result<FsTaskResultEnum, TaskError> {
 }
 
 async fn handle_write(task: &WriteTask) -> Result<FsTaskResultEnum, TaskError> {
-    todo!()
+    let output = WriteResult::new();
+
+    let root = get_root().await;
+
+    let file_name = task.path.clone().into_os_string().into_string().unwrap();
+    let mut options = FileSystemGetFileOptions::new();
+    options.create(true);
+    let file_handle_promise = root.get_file_handle_with_options(&file_name, &options);
+    let file_handle_js = JsFuture::from(file_handle_promise).await.expect("Error creating file handle");
+    let file_handle: FileSystemFileHandle = file_handle_js.try_into().expect("Failed to cast JsValue to FileSystemFileHandle");
+    let file_stream_promise = file_handle.create_writable();
+    let file_stream_js = JsFuture::from(file_stream_promise).await.expect("Error creating file stream");
+    let file_stream: FileSystemWritableFileStream = file_stream_js.try_into().expect("Failed to cast JsValue to FileSystemWritableFileStream");
+
+    Ok(FsTaskResultEnum::Write(output))
 }
 
 async fn handle_read_dir(task: &ReadDirTask) -> Result<FsTaskResultEnum, TaskError> {
@@ -66,15 +80,11 @@ async fn handle_read_dir(task: &ReadDirTask) -> Result<FsTaskResultEnum, TaskErr
         Ok(dir_handle_js) => dir_handle_js,
         Err(e) => {
             let error_string = format!("Error getting directory handle: {:?}", e);
+            // NOTE: need this error, in order to create new dir for later!
             return Err(TaskError::IoError(error_string));
         }
     };
-    let dir_handle: FileSystemDirectoryHandle = match dir_handle_js.try_into() {
-        Ok(handle) => handle,
-        Err(_) => {
-            panic!("Failed to cast JsValue to FileSystemDirectoryHandle");
-        }
-    };
+    let dir_handle: FileSystemDirectoryHandle = dir_handle_js.try_into().expect("Failed to cast JsValue to FileSystemDirectoryHandle");
 
     let dir_handle_js = JsValue::from(dir_handle);
 
@@ -84,38 +94,18 @@ async fn handle_read_dir(task: &ReadDirTask) -> Result<FsTaskResultEnum, TaskErr
         &JsValue::from("entries"),
     )
         .unwrap();
-    let entries_function: Function = match entries_function_js.try_into() {
-        Ok(function) => function,
-        Err(_) => {
-            panic!("Failed to cast JsValue to Function");
-        }
-    };
+    let entries_function: Function = entries_function_js.try_into().expect("Failed to cast JsValue to Function");
 
     // Call 'entries' method using Reflect::apply()
     let arguments_list = Array::new();
     let async_iterator_js = js_sys::Reflect::apply(&entries_function, &dir_handle_js, &arguments_list)
         .expect("Failed to call entries method");
-    let async_iterator: AsyncIterator = match async_iterator_js.try_into() {
-        Ok(iterator) => iterator,
-        Err(_) => {
-            panic!("Failed to cast JsValue to AsyncIterator");
-        }
-    };
+    let async_iterator: AsyncIterator = async_iterator_js.try_into().expect("Failed to cast JsValue to AsyncIterator");
 
     loop {
-        let next_promise = match async_iterator.next() {
-            Ok(next_promise) => next_promise,
-            Err(e) => {
-                panic!("Error getting next entry (before promise): {:?}", e);
-            }
-        };
+        let next_promise = async_iterator.next().expect("Error getting next entry (before promise)");
 
-        let next_entry = match JsFuture::from(next_promise).await {
-            Ok(value) => value,
-            Err(e) => {
-                panic!("Error getting next entry (after promise): {:?}", e);
-            }
-        };
+        let next_entry = JsFuture::from(next_promise).await.expect("Error getting next entry (after promise)");
         let done = js_sys::Reflect::get(&next_entry, &JsValue::from_str("done"))
             .unwrap()
             .as_bool()
@@ -127,21 +117,11 @@ async fn handle_read_dir(task: &ReadDirTask) -> Result<FsTaskResultEnum, TaskErr
         let value = js_sys::Reflect::get(&next_entry, &JsValue::from_str("value"))
             .unwrap();
 
-        let next_entry: Array = match value.try_into() {
-            Ok(array) => array,
-            Err(_) => {
-                panic!("Failed to cast iterator's value to Array");
-            }
-        };
+        let next_entry: Array = value.try_into().expect("Failed to cast iterator's value to Array");
         let name_js = next_entry.get(0);
         let handle_js = next_entry.get(1);
 
-        let name = match name_js.as_string() {
-            Some(name) => name,
-            None => {
-                panic!("Failed to cast JsValue to String");
-            }
-        };
+        let name = name_js.as_string().expect("Failed to cast JsValue to String");
 
         info!("Found entry: {:?}", name);
         output.add_entry(ReadDirEntry::new("".into(), name));
@@ -162,19 +142,8 @@ async fn handle_create_dir(task: &CreateDirTask) -> Result<FsTaskResultEnum, Tas
     let mut options = FileSystemGetDirectoryOptions::new();
     options.create(true);
     let dir_handle_promise = root.get_directory_handle_with_options(&folder_name, &options);
-    let dir_handle_js = match JsFuture::from(dir_handle_promise).await {
-        Ok(dir_handle_js) => dir_handle_js,
-        Err(e) => {
-            let error_string = format!("Error creating directory handle: {:?}", e);
-            return Err(TaskError::IoError(error_string));
-        }
-    };
-    let _dir_handle: FileSystemDirectoryHandle = match dir_handle_js.try_into() {
-        Ok(handle) => handle,
-        Err(_) => {
-            panic!("Failed to cast JsValue to FileSystemDirectoryHandle");
-        }
-    };
+    let dir_handle_js = JsFuture::from(dir_handle_promise).await.expect("Error creating directory handle");
+    let _dir_handle: FileSystemDirectoryHandle = dir_handle_js.try_into().expect("Failed to cast JsValue to FileSystemDirectoryHandle");
 
     Ok(FsTaskResultEnum::CreateDir(output))
 }
@@ -189,13 +158,7 @@ async fn get_root() -> FileSystemDirectoryHandle {
     let root_handle: FileSystemDirectoryHandle = match root_result {
         Ok(root_handle_js) => {
             // Attempt to cast JsValue to FileSystemDirectoryHandle
-            let root_handle: FileSystemDirectoryHandle = match root_handle_js.try_into() {
-                Ok(handle) => handle,
-                Err(_) => {
-                    panic!("Failed to cast JsValue to FileSystemDirectoryHandle");
-                }
-            };
-
+            let root_handle: FileSystemDirectoryHandle = root_handle_js.try_into().expect("Failed to cast JsValue to FileSystemDirectoryHandle");
             root_handle
         },
         Err(e) => {
