88f01aebf4426b5f0b3570560850b154c0e9fc64
Connor Carpenter <connorcarpenter@gmail.com>
2024-03-14 00:00:56 -0500

    cleanup ui editor

--- apps/ui_editor/src/app/systems/scene.rs
+++ apps/ui_editor/src/app/systems/scene.rs
@@ -26,29 +26,22 @@ use game_engine::{
     ui::Ui,
 };
 
-use crate::app::systems::ui::{bits_read_ui, bits_write_ui};
 use crate::app::{
     resources::Global,
-    systems::ui::{init_ui, json_read_ui, json_write_ui},
+    systems::ui::{init_ui, json_read_ui, json_write_ui, bits_read_ui, bits_write_ui},
 };
-
-#[derive(Component)]
-pub struct CubeMarker;
 
 pub fn scene_setup(
     mut commands: Commands,
-    mut meshes: ResMut<Storage<CpuMesh>>,
-    mut materials: ResMut<Storage<CpuMaterial>>,
     mut embedded_asset_events: EventWriter<EmbeddedAssetEvent>,
 ) {
     // TODO: use some kind of catalog here
     embedded_asset_events.send(embedded_asset_event!("../embedded/8273wa")); // palette
     embedded_asset_events.send(embedded_asset_event!("../embedded/34mvvk")); // verdana icon
 
-    let scene_camera = setup_3d_scene(&mut commands, &mut meshes, &mut materials);
     let ui_camera = setup_ui(&mut commands);
 
-    commands.insert_resource(Global::new(scene_camera, ui_camera));
+    commands.insert_resource(Global::new(ui_camera));
 }
 
 fn setup_ui(commands: &mut Commands) -> Entity {
@@ -64,9 +57,9 @@ fn setup_ui(commands: &mut Commands) -> Entity {
     let camera_id = commands
         .spawn(CameraBundle {
             camera: Camera {
-                viewport: None, // this will set later
+                viewport: None,
+                clear_operation: ClearOperation::from_rgba(0.0, 0.0, 0.0, 1.0),
                 target: RenderTarget::Screen,
-                clear_operation: ClearOperation::none(),
                 ..Default::default()
             },
             projection: Projection::Orthographic(OrthographicProjection {
@@ -97,103 +90,6 @@ fn setup_ui(commands: &mut Commands) -> Entity {
     camera_id
 }
 
-fn setup_3d_scene(
-    commands: &mut Commands,
-    meshes: &mut Storage<CpuMesh>,
-    materials: &mut Storage<CpuMaterial>,
-) -> Entity {
-    // render_layer
-    let layer = RenderLayers::layer(0);
-
-    // cube
-    commands
-        .spawn(RenderObjectBundle {
-            mesh: meshes.add(shapes::Cube),
-            material: materials.add(Color::RED),
-            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 120.0))
-                .with_scale(Vec3::splat(30.0)),
-            ..Default::default()
-        })
-        .insert(CubeMarker)
-        .insert(layer);
-
-    // ambient light
-    commands
-        .spawn(AmbientLight::new(0.1, Color::WHITE))
-        .insert(layer);
-
-    // directional light
-    let light_source = Vec3::new(-500.0, 500.0, 200.0);
-    let light_target = Vec3::ZERO;
-    commands
-        .spawn(DirectionalLight::new(
-            2.0,
-            Color::WHITE,
-            light_target - light_source,
-        ))
-        .insert(layer);
-
-    // camera
-    let camera_id = commands
-        .spawn(CameraBundle {
-            camera: Camera {
-                viewport: None,
-                clear_operation: ClearOperation::from_rgba(0.0, 0.0, 0.0, 1.0),
-                target: RenderTarget::Screen,
-                ..Default::default()
-            },
-            transform: Transform::from_xyz(400.0, 400.0, 400.0).looking_at(Vec3::ZERO, Vec3::Z),
-            projection:
-            // Projection::Orthographic(OrthographicProjection {
-            //     near: 0.1,
-            //     far: 10000.0,
-            //     ..Default::default()
-            // }),
-                Projection::Perspective(PerspectiveProjection {
-                            fov: std::f32::consts::PI / 4.0,
-                            near: 0.1,
-                            far: 10000.0,
-                           }),
-        })
-        .insert(layer)
-        .id();
-
-    camera_id
-}
-
-pub fn scene_step(
-    time: Res<Time>,
-    mut object_q: Query<&mut Transform, With<CubeMarker>>,
-    mut rotation: Local<f32>,
-) {
-    let elapsed_time = time.get_elapsed();
-
-    if *rotation == 0.0 {
-        *rotation = 0.01;
-    } else {
-        *rotation += 0.03 * elapsed_time;
-        if *rotation > 359.0 {
-            *rotation = 0.01;
-        }
-    }
-
-    for mut transform in object_q.iter_mut() {
-        // rotate
-        transform.translation.x = f32::to_radians(*rotation).cos() * 250.0;
-        transform.translation.y = f32::to_radians(*rotation).sin() * 250.0;
-        transform.translation.z = 60.0;
-
-        transform.rotate_x(0.001 * elapsed_time);
-        transform.rotate_y(0.002 * elapsed_time);
-    }
-}
-
-// TODO: handle mouse move events to update cursor (ui.update_cursor())
-// TODO: handle keypress events to update focus (ui.navigate(up/down/left/right))
-// TODO: ui.receive_click() -> Option<UiId>(); // ui determined by cursor
-// TODO: ui.receive_select() -> UiId; // ui determined by focus
-// TODO: ui.receive_char() // if textline widget is in focus
-
 pub fn scene_draw(
     mut render_frame: ResMut<RenderFrame>,
     asset_manager: Res<AssetManager>,
@@ -201,18 +97,8 @@ pub fn scene_draw(
     cameras_q: Query<(&Camera, &Transform, &Projection, Option<&RenderLayer>)>,
     // UIs
     mut uis_q: Query<(&mut Ui, Option<&RenderLayer>)>,
-    // Meshes
-    cpu_meshes_q: Query<(
-        &Handle<CpuMesh>,
-        &Handle<CpuMaterial>,
-        &Transform,
-        &Visibility,
-        Option<&RenderLayer>,
-    )>,
     // Lights
     ambient_lights_q: Query<(&AmbientLight, Option<&RenderLayer>)>,
-    point_lights_q: Query<(&PointLight, Option<&RenderLayer>)>,
-    directional_lights_q: Query<(&DirectionalLight, Option<&RenderLayer>)>,
 ) {
     // Aggregate Cameras
     for (camera, transform, projection, render_layer_opt) in cameras_q.iter() {
@@ -222,27 +108,9 @@ pub fn scene_draw(
         render_frame.draw_camera(render_layer_opt, camera, transform, projection);
     }
 
-    // Aggregate Point Lights
-    for (point_light, render_layer_opt) in point_lights_q.iter() {
-        render_frame.draw_point_light(render_layer_opt, point_light);
-    }
-
-    // Aggregate Directional Lights
-    for (dir_light, render_layer_opt) in directional_lights_q.iter() {
-        render_frame.draw_directional_light(render_layer_opt, dir_light);
-    }
-
     // Aggregate Ambient Lights
     for (ambient_light, render_layer_opt) in ambient_lights_q.iter() {
         render_frame.draw_ambient_light(render_layer_opt, ambient_light);
-    }
-
-    // Aggregate Cpu Meshes
-    for (mesh_handle, mat_handle, transform, visibility, render_layer_opt) in cpu_meshes_q.iter() {
-        if !visibility.visible {
-            continue;
-        }
-        render_frame.draw_mesh(render_layer_opt, mesh_handle, mat_handle, transform);
     }
 
     // Aggregate UIs
@@ -296,23 +164,4 @@ pub fn handle_viewport_resize(
             );
         }
     }
-
-    if let Ok((mut camera, _transform)) = cameras_q.get_mut(global.camera_3d) {
-        let should_change = if let Some(viewport) = camera.viewport.as_mut() {
-            *viewport != window_res.logical_size
-        } else {
-            true
-        };
-        if should_change {
-            let new_viewport = Viewport::new_at_origin(
-                window_res.logical_size.width,
-                window_res.logical_size.height,
-            );
-            camera.viewport = Some(new_viewport);
-
-            //info!("resize window detected. new viewport: (x: {:?}, y: {:?}, width: {:?}, height: {:?})", new_viewport.x, new_viewport.y, new_viewport.width, new_viewport.height);
-        }
-    }
-
-    // resize scene camera
 }
