b0f281f3700307fde3cddbc6420817d5495d3b35
Connor Carpenter <connorcarpenter@gmail.com>
2024-03-20 21:00:00 -0500

    gamepad events

--- apps/launcher/src/app/systems/gamepad.rs
+++ apps/launcher/src/app/systems/gamepad.rs
@@ -2,7 +2,7 @@ use bevy_ecs::event::EventReader;
 use bevy_ecs::system::{NonSend, Res};
 use bevy_log::info;
 
-use game_engine::input::{gamepad::{GamepadButtonType, GamepadAxisType, GamepadAxis, Axis, GamepadButton, Gamepads, InputGilrs}, Input};
+use game_engine::input::{gamepad::{GamepadButtonType, GamepadConnectionEvent, GamepadButtonInput, GamepadButtonChangedEvent, GamepadAxisChangedEvent, GamepadAxisType, GamepadAxis, Axis, GamepadButton, Gamepads, InputGilrs}, Input};
 use game_engine::input::{InputEvent, Key, MouseButton};
 
 pub(crate) fn gamepad_system(
@@ -12,6 +12,10 @@ pub(crate) fn gamepad_system(
     gamepads: Res<Gamepads>,
     button_inputs: NonSend<InputGilrs>,
     axes: Res<Axis<GamepadAxis>>,
+    mut gilrs_cnct_events: EventReader<GamepadConnectionEvent>,
+    mut gilrs_axis_events: EventReader<GamepadAxisChangedEvent>,
+    mut gilrs_bttn_events: EventReader<GamepadButtonChangedEvent>,
+    mut gilrs_inpt_events: EventReader<GamepadButtonInput>,
 ) {
     // winit events
     for event in winit_input_events.read() {
@@ -31,13 +35,35 @@ pub(crate) fn gamepad_system(
     let mouse_pos = winit_input.mouse_position();
     let left_btn_pressed = winit_input.is_pressed(MouseButton::Left);
 
+    // gamepad events
+    for connection_event in gilrs_cnct_events.read() {
+        info!("{:?}", connection_event);
+    }
+    for axis_changed_event in gilrs_axis_events.read() {
+        info!(
+            "{:?} of {:?} is changed to {}",
+            axis_changed_event.axis_type, axis_changed_event.gamepad, axis_changed_event.value
+        );
+    }
+    for button_changed_event in gilrs_bttn_events.read() {
+        info!(
+            "{:?} of {:?} is changed to {}",
+            button_changed_event.button_type,
+            button_changed_event.gamepad,
+            button_changed_event.value
+        );
+    }
+    for button_input_event in gilrs_inpt_events.read() {
+        info!("{:?}", button_input_event);
+    }
 
+    // gamepad state
     for gamepad in gamepads.iter() {
         if button_inputs.is_pressed(GamepadButton::new(gamepad, GamepadButtonType::RightBumper)) {
             info!("{:?} RightBumper pressed", gamepad);
         } else if !button_inputs.is_pressed(GamepadButton::new(gamepad, GamepadButtonType::RightBumper))
         {
-            info!("{:?} RightBumper not pressed", gamepad);
+            //info!("{:?} RightBumper not pressed", gamepad);
         }
 
         let right_trigger = axes
