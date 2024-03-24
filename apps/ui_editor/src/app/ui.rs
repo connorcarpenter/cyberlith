f028aa41bd14dc5073cd05ef5ae402e892aee437
Connor Carpenter <connorcarpenter@gmail.com>
2024-03-23 19:26:11 -0500

    stubbing out register ui

--- apps/ui_editor/src/app/ui.rs
+++ apps/ui_editor/src/app/ui.rs
@@ -25,7 +25,10 @@ fn ui_define() -> (String, AssetId, ETag, Ui) {
     //return start::ui_define();
 
     // login
-    return login::ui_define();
+    //return login::ui_define();
+
+    // register
+    return register::ui_define();
 }
 
 #[derive(Event, Default)]
--- apps/ui_editor/src/app/ui_backups/mod.rs
+++ apps/ui_editor/src/app/ui_backups/mod.rs
@@ -1,2 +1,3 @@
 pub mod start;
 pub mod login;
+pub mod register;
