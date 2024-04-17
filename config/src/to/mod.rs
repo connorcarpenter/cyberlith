e25b5d0e5a4de7326e69160a6ba3199eb8baf439
Connor Carpenter <connorcarpenter@gmail.com>
2024-04-17 15:42:20 -0500

    restructure cfg_if in config to allow for code completion

--- config/src/to/mod.rs
+++ config/src/to/mod.rs
@@ -3,35 +3,53 @@ cfg_if! {
         mod client;
         pub use self::client::*;
     }
-    else if #[cfg(feature = "gateway")] {
+}
+
+cfg_if! {
+    if #[cfg(feature = "gateway")] {
         mod gateway;
         pub use self::gateway::*;
     }
-    else if #[cfg(feature = "region")] {
+}
+
+cfg_if! {
+    if #[cfg(feature = "region")] {
         mod region;
         pub use self::region::*;
     }
-    else if #[cfg(feature = "session")] {
+}
+
+cfg_if! {
+    if #[cfg(feature = "session")] {
         mod session;
         pub use self::session::*;
     }
-    else if #[cfg(feature = "world")] {
+}
+
+cfg_if! {
+    if #[cfg(feature = "world")] {
         mod world;
         pub use self::world::*;
     }
-    else if #[cfg(feature = "content")] {
+}
+
+cfg_if! {
+    if #[cfg(feature = "content")] {
         mod content;
         pub use self::content::*;
     }
-    else if #[cfg(feature = "asset")] {
+}
+
+cfg_if! {
+    if #[cfg(feature = "asset")] {
         mod asset;
         pub use self::asset::*;
     }
-    else if #[cfg(feature = "auth")] {
+}
+
+cfg_if! {
+    if #[cfg(feature = "auth")] {
         mod auth;
         pub use self::auth::*;
-    }
-    else {
-        compile_error!("Required to specify a feature flag for the target environment, either 'local' or 'prod'");
     }
 }
