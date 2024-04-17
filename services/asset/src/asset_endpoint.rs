844337e0462db2d89d1c503f56fb472669984018
Connor Carpenter <connorcarpenter@gmail.com>
2024-04-17 16:56:22 -0500

    get other services working with new api

--- services/asset/src/asset_endpoint.rs
+++ services/asset/src/asset_endpoint.rs
@@ -1,7 +1,7 @@
 use logging::info;
 
 use http_client::ResponseError;
-use http_server::{async_dup::Arc, smol::lock::RwLock, Server};
+use http_server::{async_dup::Arc, smol::lock::RwLock, Server, ApiServer};
 
 use asset_server_http_proto::{AssetRequest, AssetResponse};
 
