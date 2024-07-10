2c2685cb4ec506b4bf235875fc50e859a8c95d40
Connor Carpenter <connorcarpenter@gmail.com>
2024-07-10 11:14:56 -0600

    don't need long-running tasks all in their own task in social server

--- services/social/src/session_servers/processes.rs
+++ services/social/src/session_servers/processes.rs
@@ -1,72 +1,29 @@
-use std::{thread, time::Duration};
-use std::collections::HashMap;
+use std::{collections::HashMap, time::Duration};
 
 use http_client::HttpClient;
-use http_server::{async_dup::Arc, executor::smol::lock::RwLock, Server};
+use http_server::{async_dup::Arc, executor::smol::{lock::RwLock, Timer}, Server};
 use logging::{info, warn};
 
-use session_server_http_proto::{SocialPatchGlobalChatMessagesRequest, SocialPatchUsersRequest, SocialUserPatch};
+use session_server_http_proto::{SocialLobbyPatch, SocialPatchGlobalChatMessagesRequest, SocialPatchMatchLobbiesRequest, SocialPatchUsersRequest, SocialUserPatch};
 
 use config::SOCIAL_SERVER_GLOBAL_SECRET;
-use crate::session_servers::SessionServerId;
 
-use crate::state::State;
-use crate::users::UserPatch;
+use crate::{match_lobbies::LobbyPatch, users::UserPatch, state::State, session_servers::SessionServerId};
 
 pub fn start_processes(state: Arc<RwLock<State>>) {
-    let state_clone = state.clone();
+
+    let state_clone_1 = state.clone();
+
+    // patches
     Server::spawn(async move {
         loop {
-            let state_clone_2 = state_clone.clone();
-            send_patches(state_clone_2).await;
-            thread::sleep(Duration::from_secs(5));
+            let state = &mut state_clone_1.write().await;
+            handle_user_patches(state).await;
+            handle_global_chat_patches(state).await;
+            handle_match_lobby_patches(state).await;
+            Timer::after(Duration::from_secs(5)).await;
         }
     });
-}
-
-async fn send_patches(state: Arc<RwLock<State>>) {
-    let state = &mut state.write().await;
-
-    handle_global_chat_patches(state).await;
-    handle_user_patches(state).await;
-}
-
-async fn handle_global_chat_patches(state: &mut State) {
-    let global_chat_patches = state.global_chat.take_patches();
-    for (sending_session_server_id, messages) in global_chat_patches {
-        for receiving_session_server_id in state.session_servers.all_session_ids() {
-            if sending_session_server_id == receiving_session_server_id {
-                continue;
-            }
-
-            let (recv_addr, recv_port) = state
-                .session_servers
-                .get_recv_addr(receiving_session_server_id)
-                .unwrap();
-
-            let request = SocialPatchGlobalChatMessagesRequest::new(
-                SOCIAL_SERVER_GLOBAL_SECRET,
-                messages.clone(),
-            );
-            let response = HttpClient::send(recv_addr, recv_port, request).await;
-            match response {
-                Ok(_) => {
-                    info!(
-                        "from {:?}:{} - global chat patch messages sent",
-                        recv_addr, recv_port
-                    );
-                }
-                Err(e) => {
-                    warn!(
-                        "from {:?}:{} - global chat patch messages send failed: {:?}",
-                        recv_addr,
-                        recv_port,
-                        e.to_string()
-                    );
-                }
-            }
-        }
-    }
 }
 
 async fn handle_user_patches(state: &mut State) {
@@ -123,6 +80,89 @@ async fn handle_user_patches(state: &mut State) {
                     recv_port,
                     e.to_string()
                 );
+            }
+        }
+    }
+}
+
+async fn handle_global_chat_patches(state: &mut State) {
+    let global_chat_patches = state.global_chat.take_patches();
+    for (sending_session_server_id, messages) in global_chat_patches {
+        for receiving_session_server_id in state.session_servers.all_session_ids() {
+            if sending_session_server_id == receiving_session_server_id {
+                continue;
+            }
+
+            let (recv_addr, recv_port) = state
+                .session_servers
+                .get_recv_addr(receiving_session_server_id)
+                .unwrap();
+
+            let request = SocialPatchGlobalChatMessagesRequest::new(
+                SOCIAL_SERVER_GLOBAL_SECRET,
+                messages.clone(),
+            );
+            let response = HttpClient::send(recv_addr, recv_port, request).await;
+            match response {
+                Ok(_) => {
+                    info!(
+                        "from {:?}:{} - global chat patch messages sent",
+                        recv_addr, recv_port
+                    );
+                }
+                Err(e) => {
+                    warn!(
+                        "from {:?}:{} - global chat patch messages send failed: {:?}",
+                        recv_addr,
+                        recv_port,
+                        e.to_string()
+                    );
+                }
+            }
+        }
+    }
+}
+
+async fn handle_match_lobby_patches(state: &mut State) {
+    let match_lobby_patches = state.match_lobbies.take_patches();
+    for (sending_session_server_id, patches) in match_lobby_patches {
+        for receiving_session_server_id in state.session_servers.all_session_ids() {
+            if sending_session_server_id == receiving_session_server_id {
+                continue;
+            }
+
+            let (recv_addr, recv_port) = state
+                .session_servers
+                .get_recv_addr(receiving_session_server_id)
+                .unwrap();
+
+            let patches = patches.iter().map(|patch| {
+                match patch {
+                    LobbyPatch::Create(lobby_id, creator_user_id, match_name) => SocialLobbyPatch::Create(lobby_id.clone(), match_name.clone(), creator_user_id.clone()),
+                    LobbyPatch::Delete(lobby_id) => SocialLobbyPatch::Delete(lobby_id.clone()),
+                }
+            }).collect();
+
+            let request = SocialPatchMatchLobbiesRequest::new(
+                SOCIAL_SERVER_GLOBAL_SECRET,
+                patches,
+            );
+            let response = HttpClient::send(recv_addr, recv_port, request).await;
+            match response {
+                Ok(_) => {
+                    info!(
+                        "from {:?}:{} - match lobbies patches sent",
+                        recv_addr, recv_port
+                    );
+                }
+                Err(e) => {
+                    warn!(
+                        "from {:?}:{} - match lobbies patches send failed: {:?}",
+                        recv_addr,
+                        recv_port,
+                        e.to_string()
+                    );
+                }
             }
         }
     }
