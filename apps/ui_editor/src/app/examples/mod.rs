850797f0b2127f1df37d0cf60625293d7fea3087
Connor Carpenter <connorcarpenter@gmail.com>
2024-07-12 14:27:27 -0600

    ui_editor reorg

--- apps/ui_editor/src/app/examples/mod.rs
+++ apps/ui_editor/src/app/examples/mod.rs
@@ -1,9 +1,6 @@
-mod global_chats_state;
-pub(crate) use global_chats_state::GlobalChatState;
-mod global_chats;
-pub(crate) use global_chats::setup_global_chat_test_case;
 
-mod user_list_state;
-pub(crate) use user_list_state::UserListState;
+mod global_chats;
+pub(crate) use global_chats::*;
+
 mod user_list;
-pub(crate) use user_list::setup_user_list_test_case;
+pub(crate) use user_list::*;
