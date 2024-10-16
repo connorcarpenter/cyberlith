33712c4a12aa7bb7c6eb5891bf117fff50b87536
Connor Carpenter <connorcarpenter@gmail.com>
2024-10-15 21:57:05 -0600

    new system works a lot better!

--- apps/game/inworld/src/resources/input_manager.rs
+++ apps/game/inworld/src/resources/input_manager.rs
@@ -56,6 +56,7 @@ impl InputManager {
 
     fn update_pressed_keys(&mut self, client_instant: GameInstant, input: &Input) -> Vec<(Key, u16)> {
         let mut output = Vec::new();
+
         for key in &self.tracked_keys {
             if input.is_pressed(*key) {
                 if let Some((prev_instant, prev_duration)) = self.pressed_keys.get_mut(key) {
@@ -95,107 +96,127 @@ impl InputManager {
 
         let next_command = self.next_command.as_mut().unwrap();
 
-        let mut hold_u = false;
-        let mut hold_d = false;
-        let mut hold_l = false;
-        let mut hold_r = false;
 
-        let mut tap_u = false;
-        let mut tap_d = false;
-        let mut tap_l = false;
-        let mut tap_r = false;
-        for (key, duration) in releases {
-            if duration < 150 {
-                // this was a tap
-                match key {
-                    Key::W => tap_u = true,
-                    Key::S => tap_d = true,
-                    Key::A => tap_l = true,
-                    Key::D => tap_r = true,
-                    _ => {},
-                }
-            } else {
-                match key {
-                    Key::W => hold_u = true,
-                    Key::S => hold_d = true,
-                    Key::A => hold_l = true,
-                    Key::D => hold_r = true,
-                    _ => {},
-                }
-            }
-        }
-        if !hold_u {
-            if let Some((_, duration)) = self.pressed_keys.get(&Key::W) {
-                if *duration >= 150 {
-                    hold_u = true;
-                }
-            }
-        }
-        if !hold_d {
-            if let Some((_, duration)) = self.pressed_keys.get(&Key::S) {
-                if *duration >= 150 {
-                    hold_d = true;
-                }
-            }
-        }
-        if !hold_l {
-            if let Some((_, duration)) = self.pressed_keys.get(&Key::A) {
-                if *duration >= 150 {
-                    hold_l = true;
-                }
-            }
-        }
-        if !hold_r {
-            if let Some((_, duration)) = self.pressed_keys.get(&Key::D) {
-                if *duration >= 150 {
-                    hold_r = true;
-                }
-            }
-        }
 
-        if tap_u && tap_d {
-            tap_u = false;
-            tap_d = false;
-        }
-        if tap_l && tap_r {
-            tap_l = false;
-            tap_r = false;
-        }
-        if hold_u && hold_d {
-            hold_u = false;
-            hold_d = false;
-        }
-        if hold_l && hold_r {
-            hold_l = false;
-            hold_r = false;
-        }
+        // Looks
+        {
+            let mut tap_u = false;
+            let mut tap_d = false;
+            let mut tap_l = false;
+            let mut tap_r = false;
+            for (key, duration) in releases {
+                if duration < 150 {
+                    // this was a tap
+                    match key {
+                        Key::W => tap_u = true,
+                        Key::S => tap_d = true,
+                        Key::A => tap_l = true,
+                        Key::D => tap_r = true,
+                        _ => {},
+                    }
+                }
+            }
 
-        // Looking
-        match (tap_u, tap_d, tap_l, tap_r) {
-            (false, false, false, true ) => next_command.set_look(Direction::East),
-            (true , false, false, true ) => next_command.set_look(Direction::Northeast),
-            (true , false, false, false) => next_command.set_look(Direction::North),
-            (true , false, true , false) => next_command.set_look(Direction::Northwest),
-            (false, false, true , false) => next_command.set_look(Direction::West),
-            (false, true , true , false) => next_command.set_look(Direction::Southwest),
-            (false, true , false, false) => next_command.set_look(Direction::South),
-            (false, true , false, true ) => next_command.set_look(Direction::Southeast),
-            (false, false, false, false) => {},
-            _ => panic!("Invalid look command"),
+            if tap_u && tap_d {
+                tap_u = false;
+                tap_d = false;
+            }
+            if tap_l && tap_r {
+                tap_l = false;
+                tap_r = false;
+            }
+
+            match (tap_u, tap_d, tap_l, tap_r) {
+                (false, false, false, true ) => next_command.set_look(Direction::East),
+                (true , false, false, true ) => next_command.set_look(Direction::Northeast),
+                (true , false, false, false) => next_command.set_look(Direction::North),
+                (true , false, true , false) => next_command.set_look(Direction::Northwest),
+                (false, false, true , false) => next_command.set_look(Direction::West),
+                (false, true , true , false) => next_command.set_look(Direction::Southwest),
+                (false, true , false, false) => next_command.set_look(Direction::South),
+                (false, true , false, true ) => next_command.set_look(Direction::Southeast),
+                (false, false, false, false) => {},
+                _ => panic!("Invalid look command"),
+            }
         }
 
         // Movement
-        match (hold_u, hold_d, hold_l, hold_r) {
-            (false, false, false, true ) => next_command.set_move(Direction::East),
-            (true , false, false, true ) => next_command.set_move(Direction::Northeast),
-            (true , false, false, false) => next_command.set_move(Direction::North),
-            (true , false, true , false) => next_command.set_move(Direction::Northwest),
-            (false, false, true , false) => next_command.set_move(Direction::West),
-            (false, true , true , false) => next_command.set_move(Direction::Southwest),
-            (false, true , false, false) => next_command.set_move(Direction::South),
-            (false, true , false, true ) => next_command.set_move(Direction::Southeast),
-            (false, false, false, false) => {},
-            _ => panic!("Invalid move command"),
+        {
+            let mut short_u = false;
+            let mut short_d = false;
+            let mut short_l = false;
+            let mut short_r = false;
+
+            let mut long_u = false;
+            let mut long_d = false;
+            let mut long_l = false;
+            let mut long_r = false;
+
+            if !long_u {
+                if let Some((_, duration)) = self.pressed_keys.get(&Key::W) {
+                    short_u = true;
+                    if *duration >= 150 {
+                        long_u = true;
+                    }
+                }
+            }
+            if !long_d {
+                if let Some((_, duration)) = self.pressed_keys.get(&Key::S) {
+                    short_d = true;
+                    if *duration >= 150 {
+                        long_d = true;
+                    }
+                }
+            }
+            if !long_l {
+                if let Some((_, duration)) = self.pressed_keys.get(&Key::A) {
+                    short_l = true;
+                    if *duration >= 150 {
+                        long_l = true;
+                    }
+                }
+            }
+            if !long_r {
+                if let Some((_, duration)) = self.pressed_keys.get(&Key::D) {
+                    short_r = true;
+                    if *duration >= 150 {
+                        long_r = true;
+                    }
+                }
+            }
+
+            if short_u && short_d {
+                short_u = false;
+                short_d = false;
+            }
+            if short_l && short_r {
+                short_l = false;
+                short_r = false;
+            }
+            if long_u && long_d {
+                long_u = false;
+                long_d = false;
+            }
+            if long_l && long_r {
+                long_l = false;
+                long_r = false;
+            }
+
+            // Movement
+            if long_u || long_d || long_l || long_r {
+                match (short_u, short_d, short_l, short_r) {
+                    (false, false, false, true) => next_command.set_move(Direction::East),
+                    (true, false, false, true) => next_command.set_move(Direction::Northeast),
+                    (true, false, false, false) => next_command.set_move(Direction::North),
+                    (true, false, true, false) => next_command.set_move(Direction::Northwest),
+                    (false, false, true, false) => next_command.set_move(Direction::West),
+                    (false, true, true, false) => next_command.set_move(Direction::Southwest),
+                    (false, true, false, false) => next_command.set_move(Direction::South),
+                    (false, true, false, true) => next_command.set_move(Direction::Southeast),
+                    (false, false, false, false) => {},
+                    _ => panic!("Invalid move command"),
+                }
+            }
         }
     }
 
@@ -204,22 +225,20 @@ impl InputManager {
     }
 
     pub fn save_to_command_history(&mut self, client_tick: Tick, command_opt: Option<PlayerCommands>) {
-        {
-            if !self.command_history.can_insert(&client_tick) {
+        if !self.command_history.can_insert(&client_tick) {
 
-                let most_recent_command_tick = self.command_history.most_recent_tick().unwrap();
+            let most_recent_command_tick = self.command_history.most_recent_tick().unwrap();
 
-                // History is full, should this be possible??
-                panic!(
-                    "Command History is full, cannot insert command for tick: {:?}, (most recent tick is {:?})",
-                    client_tick,
-                    most_recent_command_tick,
-                );
-            }
-
-            // Record command
-            self.command_history.insert(client_tick, command_opt.clone());
+            // History is full, should this be possible??
+            panic!(
+                "Command History is full, cannot insert command for tick: {:?}, (most recent tick is {:?})",
+                client_tick,
+                most_recent_command_tick,
+            );
         }
+
+        // Record command
+        self.command_history.insert(client_tick, command_opt.clone());
     }
 
     pub fn pop_command_replays(&mut self, server_tick: Tick) -> Vec<(Tick, Option<PlayerCommands>)> {
